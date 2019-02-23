574d7b8c68c123701ef6c12cc75e4ab69947972f
Vincent Kwok <vkwok22@gmail.com>
2019-02-22 21:11:23 -0600

    prepare for heroku deploy

--- config/puma.rb
+++ config/puma.rb
@@ -4,11 +4,13 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
+rackup      DefaultRackup
 port        ENV['PORT'] || 3000
 
 # Specifies the `environment` that Puma will run in.
@@ -30,7 +32,7 @@ environment ENV['RAILS_ENV'] || 'development'
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
 #
-# preload_app!
+preload_app!
 
 # If you are preloading your application and using Active Record, it's
 # recommended that you close any connections to the database before workers
@@ -47,9 +49,9 @@ environment ENV['RAILS_ENV'] || 'development'
 # or connections that may have been created at application boot, as Ruby
 # cannot share connections between processes.
 #
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-# end
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
 #
 
 # Allow puma to be restarted by `rails restart` command.
